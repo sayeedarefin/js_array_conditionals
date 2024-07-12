aa6cea3121701980010f9ea33189f8431c53da1a
sayeedarefin <sayeedarefin5@gmail.com>
2024-07-12 10:21:33 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,11 @@
+var prices = [100, 23, 45, 564, 5, 69, 66];
+var names = ["sayeed", "arefin", "rasik", "kashem", "shofi"];
+var letter = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(prices);
+console.log(names);
+console.log(letter);
+
+console.log(prices.length);
+console.log(names.length);
+console.log(letter.length);
